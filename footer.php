1a551cf34c53ba2cfe48d5f35d8b43219b93bf62
chenjunlin <3222391319@qq.com>
2021-07-27 22:46:40 +0800

    添加ajax请求

--- footer.php
+++ footer.php
@@ -11,6 +11,24 @@
        </div>
     </div>
 </footer>
+
+<div style="display:none; z-index:999; background-color: #ccc; position:fixed; top:0px; width:100%; height:100vh;" class="pjax_main">
+</div>
+<script>
+    $(document).pjax('a', '#body', {
+        fragment:'#body', timeout:1000
+    }); 
+    $(document).on('pjax:send', function() { 
+        $(".pjax_main").css("display", "block");
+    });
+    $(document).on('pjax:complete', function() { 
+        $(".pjax_main").css("display", "none");
+        
+    });
+</script>
+
+
+
 <script charset="utf-8" src="<?php $this->options->themeUrl('main.js'); ?>"></script>
 
 
